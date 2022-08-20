92548eb9052c2d6d20c17d6ae861761cda26ce9b
Seth Axen <seth.axen@gmail.com>
2022-08-21 01:36:41 +0200

    Update docstring

--- src/load.jl
+++ src/load.jl
@@ -15,16 +15,16 @@ A file is downloaded only when it is requested and then cached for future use.
 
 ```jldoctest
 julia> keys(load_example_data())
-KeySet for a Dict{String, ArviZ.AbstractFileMetadata} with 9 entries. Keys:
+KeySet for a OrderedCollections.OrderedDict{String, ArviZExampleData.AbstractFileMetadata} with 9 entries. Keys:
   "centered_eight"
-  "radon"
-  "glycan_torsion_angles"
-  "rugby"
   "non_centered_eight"
+  "radon"
+  "rugby"
+  "regression1d"
   "regression10d"
   "classification1d"
   "classification10d"
-  "regression1d"
+  "glycan_torsion_angles"
 
 julia> load_example_data("centered_eight")
 InferenceData with groups:
