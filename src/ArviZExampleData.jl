f8e0edd18d78b6e6f2a483faeabb9bd37170816c
Seth Axen <seth.axen@gmail.com>
2022-08-20 20:56:59 +0200

    Add more dependencies

--- src/ArviZExampleData.jl
+++ src/ArviZExampleData.jl
@@ -1,6 +1,9 @@
 module ArviZExampleData
 
+using DataDeps: DataDeps
 using InferenceObjects: InferenceObjects
+using JSON3: JSON3
 using NCDatasets: NCDatasets
+using StructTypes: StructTypes
 
 end
