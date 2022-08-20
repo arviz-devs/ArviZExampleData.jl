53ad3e4e9ccef635f214bde393cc613c4767ec6d
Seth Axen <seth.axen@gmail.com>
2022-08-21 00:41:19 +0200

    Define constants

--- src/ArviZExampleData.jl
+++ src/ArviZExampleData.jl
@@ -6,4 +6,11 @@ using JSON3: JSON3
 using NCDatasets: NCDatasets
 using StructTypes: StructTypes
 
+const DEPS_DATA_DIR = joinpath(pkgdir(ArviZExampleData), "deps", "data")
+const EXAMPLE_DATA_DIR = joinpath(DEPS_DATA_DIR, "example_data")
+const LOCAL_DATA_DIR = joinpath(EXAMPLE_DATA_DIR, "data")
+const LOCAL_DATA_FILE = joinpath(EXAMPLE_DATA_DIR, "data_local.json")
+const REMOTE_DATA_FILE = joinpath(EXAMPLE_DATA_DIR, "data_remote.json")
+const DATA_HTTP_PROTOCOL = "https"
+
 end
