409293e1a3abf8f87c5dcf324c7d73b046611edf
Seth Axen <seth@sethaxen.com>
2026-01-27 16:49:41 +0100

    fix: Use http instead of https in URLs

--- src/register.jl
+++ src/register.jl
@@ -2,7 +2,8 @@ function register_data(md::LocalFileMetadata)
     return DataDeps.register(DataDeps.ManualDataDep(md.name, md.description))
 end
 function register_data(md::RemoteFileMetadata)
-    return DataDeps.register(DataDeps.DataDep(md.name, md.description, md.url, md.checksum))
+    url = replace(md.url, "https" => "http")
+    return DataDeps.register(DataDeps.DataDep(md.name, md.description, url, md.checksum))
 end
 
 register_all_data() = map(register_data, values(REMOTE_EXAMPLE_DATA))
